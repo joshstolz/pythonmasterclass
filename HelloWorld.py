9b053368adc2487ad1df09390fe6f65f06477af9
joshstolz <62106682+joshstolz@users.noreply.github.com>
2022-02-14 21:46:44 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+print('Hello, World!')
+print(1 + 2)
+print(7 * 6)
+print("The end","or is it?", "keept watchin to learn more about Python", 3)
---
+++ strings.py
@@ -0,0 +1,20 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world ")
+greeting = "Hello"
+name = "Tim"
+
+
+print(greeting + " " + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
